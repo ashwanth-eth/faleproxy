da33f5fd560638fb4c0cbe344eb12df2966988b7
Ashwanth Samuel <ashwanthsamuel@gmail.com>
2025-11-10 17:15:30 -0500

    Fix integration tests: use PORT environment variable instead of file modification
    - Remove file copying and sed commands that don't work on Linux
    - Use PORT environment variable when spawning test server
    - This works cross-platform and is cleaner than modifying files

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,8 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { exec } = require('child_process');
-const { promisify } = require('util');
-const execAsync = promisify(exec);
+const { spawn } = require('child_process');
 const { sampleHtmlWithYale } = require('./test-utils');
 const nock = require('nock');
 
@@ -11,20 +9,17 @@ const TEST_PORT = 3099;
 let server;
 
 describe('Integration Tests', () => {
-  // Modify the app to use a test port
+  // Start the app server with a test port
   beforeAll(async () => {
     // Mock external HTTP requests
     nock.disableNetConnect();
     nock.enableNetConnect('127.0.0.1');
     
-    // Create a temporary test app file
-    await execAsync('cp app.js app.test.js');
-    await execAsync(`sed -i '' 's/const PORT = 3001/const PORT = ${TEST_PORT}/' app.test.js`);
-    
-    // Start the test server
-    server = require('child_process').spawn('node', ['app.test.js'], {
+    // Start the test server with PORT environment variable
+    server = spawn('node', ['app.js'], {
       detached: true,
-      stdio: 'ignore'
+      stdio: 'ignore',
+      env: { ...process.env, PORT: TEST_PORT.toString() }
     });
     
     // Give the server time to start
@@ -34,9 +29,12 @@ describe('Integration Tests', () => {
   afterAll(async () => {
     // Kill the test server and clean up
     if (server && server.pid) {
-      process.kill(-server.pid);
+      try {
+        process.kill(-server.pid);
+      } catch (error) {
+        // Server may have already exited
+      }
     }
-    await execAsync('rm app.test.js');
     nock.cleanAll();
     nock.enableNetConnect();
   });
